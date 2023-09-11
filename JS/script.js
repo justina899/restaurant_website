7bc75c04abbdfa2ef8c21a70972c889370df9e4c
Justina <justina.babrauskiene@gmail.com>
2023-09-11 15:31:59 +0300

    Fixed Mobile Nav Menu Automatic Collapse

--- JS/script.js
+++ JS/script.js
@@ -1,8 +1,8 @@
-$(function(){
-    $("#navbar-toggler").blur(function(event){
+$(function() {
+    $("#navbarToggle").blur(function(event) {
         var screenWidth = window.innerWidth;    //window.innerWidth - screen of the browser
-        if(screenWidth < 768){
-            $("#collapsable-nav").collapse({ 'toggle': false }).collapse('hide');
+        if(screenWidth < 768) {
+            $("#collapsable-nav").collapse('hide');
         }
     });
 });
